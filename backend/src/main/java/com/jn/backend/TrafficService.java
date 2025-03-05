53c01860c12bd4a741df663e956bf71cda2d0ac2
Jakub Nowak <njakub002@gmail.com>
2025-03-05 21:01:52 +0100

    Backend - lights changes optimized

--- backend/src/main/java/com/jn/backend/TrafficService.java
+++ backend/src/main/java/com/jn/backend/TrafficService.java
@@ -5,7 +5,6 @@ import com.jn.backend.dto.ResponseDto;
 import com.jn.backend.enums.RoadDirection;
 import com.jn.backend.model.*;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 @org.springframework.stereotype.Service
@@ -26,51 +25,57 @@ public class TrafficService {
         for (RoadDirection direction : RoadDirection.values()) {
             vehicleQueues.put(direction, new LinkedList<>());
         }
-        stepsNumberMap.put(true, 5); // NS
-        stepsNumberMap.put(false, 5); // WE
+        stepsNumberMap.put(true, 0); // NS
+        stepsNumberMap.put(false, 0); // WE
     }
 
-    private int countVehiclesInDirection(boolean isNS) {
-        return (isNS ? List.of(
-                RoadDirection.NORTH, RoadDirection.SOUTH)
-                : List.of(RoadDirection.EAST, RoadDirection.WEST))
-                .stream()
-                .mapToInt(dir -> vehicleQueues.get(dir).size())
-                .sum();
+    public ResponseDto processSimulation(RequestDto requestData) {
+        ResponseDto resultDto = new ResponseDto();
+        resetLightsState();
+        for (Command command : requestData.getCommands()) {
+            command.execute(this, resultDto);
+        }
+
+        return resultDto;
     }
 
-    private boolean isCrossEmpty() {
-        return vehicleQueues.values().stream().allMatch(Queue::isEmpty);
+    @SuppressWarnings("ReassignedVariable")
+    public List<String> nextTrafficStep() {
+        if (isCrossEmpty()) {
+            return Collections.emptyList();
+        } else {
+            if(stepsNumberMap.get(isNS) == 0 && stepsNumberMap.get(!isNS) == 0){
+                updateLightsState();
+                changeLights();
+            }
+            if(stepsNumberMap.get(isNS) == 0){
+                changeLights();
+            }
+
+            List<RoadDirection> activeDirections = getRoadDirectionsList(isNS);
+            if (areQueuesEmpty(activeDirections)) {
+                stepsNumberMap.put(isNS, 0);
+                changeLights();
+                activeDirections = getRoadDirectionsList(isNS);
+            }
+
+            return moveVehiclesOffTheRoad(
+                    vehicleQueues.get(activeDirections.get(0)),
+                    vehicleQueues.get(activeDirections.get(1))
+            );
+        }
     }
 
-    private int countDirectionStepsNumber(int totalVehicles, int directionVehicles) {
-        return (totalVehicles == 0) ? 0
-                : (int) Math.round(((double) directionVehicles / totalVehicles) * maxSteps);
+    public void addVehicle(DestinationVehicle vehicle) {
+        vehicleQueues.get(vehicle.getStartRoad()).add(vehicle);
     }
 
-    private List<String> processVehicles() {
-        List<RoadDirection> activeDirections = isNS ? List.of(RoadDirection.NORTH, RoadDirection.SOUTH)
-                : List.of(RoadDirection.EAST, RoadDirection.WEST);
-
-        Queue<DestinationVehicle> queueA = vehicleQueues.get(activeDirections.get(0));
-        Queue<DestinationVehicle> queueB = vehicleQueues.get(activeDirections.get(1));
-
+    private List<String> moveVehiclesOffTheRoad(Queue<DestinationVehicle> queueB, Queue<DestinationVehicle> queueA) {
+        List<String> result = new ArrayList<>();
         DestinationVehicle vehicleA = queueA.peek();
         DestinationVehicle vehicleB = queueB.peek();
 
-        stepsNumberMap.put(isNS, stepsNumberMap.get(isNS) - 1);
-
-        return moveVehiclesOffTheRoad(vehicleA, vehicleB, queueB, queueA);
-    }
-
-    private List<String> moveVehiclesOffTheRoad(DestinationVehicle vehicleA, DestinationVehicle vehicleB,
-                                                Queue<DestinationVehicle> queueB, Queue<DestinationVehicle> queueA) {
-        List<String> result = new ArrayList<>();
-
-        if (vehicleA == null && vehicleB == null) {
-            stepsNumberMap.put(isNS, 0);
-            changeLights();
-        }else if (vehicleA == null) {
+        if (vehicleA == null) {
             queueB.poll();
             result.add(vehicleB.getVehicleId());
         } else if (vehicleB == null) {
@@ -79,11 +84,12 @@ public class TrafficService {
         }else{
             result.addAll(handleVehicleDirections(vehicleA, vehicleB, queueA, queueB));
         }
+        stepsNumberMap.put(isNS, stepsNumberMap.get(isNS) - 1);
         return result;
     }
 
     private List<String> handleVehicleDirections(DestinationVehicle vehicleA, DestinationVehicle vehicleB,
-                                         Queue<DestinationVehicle> queueA, Queue<DestinationVehicle> queueB) {
+                                                 Queue<DestinationVehicle> queueA, Queue<DestinationVehicle> queueB) {
         List<String> result = new ArrayList<>();
         if (isCollision(vehicleA.getEndRoad(), vehicleB.getEndRoad())){
             queueA.poll();
@@ -99,6 +105,47 @@ public class TrafficService {
         return result;
     }
 
+    private void updateLightsState() {
+        int allVehicles = countVehiclesInDirection(true) + countVehiclesInDirection(false);
+        stepsNumberMap.put(true, countDirectionStepsNumber(allVehicles, countVehiclesInDirection(true)));
+        stepsNumberMap.put(false, countDirectionStepsNumber(allVehicles, countVehiclesInDirection(false)));
+    }
+
+    private void resetLightsState(){
+        stepsNumberMap.put(true, 0);
+        stepsNumberMap.put(false, 0);
+        for (RoadDirection direction : RoadDirection.values()) {
+            vehicleQueues.replace(direction, new LinkedList<>());
+        }
+    }
+
+    private int countVehiclesInDirection(boolean isNS) {
+        return getRoadDirectionsList(isNS)
+                .stream()
+                .mapToInt(dir -> vehicleQueues.get(dir).size())
+                .sum();
+    }
+
+    private static List<RoadDirection> getRoadDirectionsList(boolean isNS) {
+        return isNS ? List.of(
+                RoadDirection.NORTH, RoadDirection.SOUTH)
+                : List.of(RoadDirection.EAST, RoadDirection.WEST);
+    }
+
+    private boolean isCrossEmpty() {
+        return vehicleQueues.values().stream().allMatch(Queue::isEmpty);
+    }
+
+    private boolean areQueuesEmpty(List<RoadDirection> directions) {
+        return vehicleQueues.get(directions.get(0)).isEmpty()
+                && vehicleQueues.get(directions.get(1)).isEmpty();
+    }
+
+    private int countDirectionStepsNumber(int totalVehicles, int directionVehicles) {
+        return (totalVehicles == 0) ? 0
+                : (int) Math.round(((double) directionVehicles / totalVehicles) * maxSteps);
+    }
+
     private boolean isCollision(RoadDirection key, RoadDirection value) {
         return COLLISION_PAIRS.containsKey(key) && COLLISION_PAIRS.get(key).equals(value);
     }
@@ -107,39 +154,6 @@ public class TrafficService {
         isNS = !isNS;
     }
 
-    private void updateTrafficInfo() {
-        int allVehicles = countVehiclesInDirection(true) + countVehiclesInDirection(false);
-        stepsNumberMap.put(true, countDirectionStepsNumber(allVehicles, countVehiclesInDirection(true)));
-        stepsNumberMap.put(false, countDirectionStepsNumber(allVehicles, countVehiclesInDirection(false)));
-    }
-
-    public List<String> nextTrafficStep() {
-        if (isCrossEmpty()) {
-            return Collections.emptyList();
-        } else {
-            if(stepsNumberMap.get(isNS) == 0 && stepsNumberMap.get(!isNS) == 0){
-                updateTrafficInfo();
-                changeLights();
-            }
-            if(stepsNumberMap.get(isNS) == 0){
-                changeLights();
-            }
-            return processVehicles();
-        }
-    }
-
-    public void addVehicle(DestinationVehicle vehicle) {
-        vehicleQueues.get(vehicle.getStartRoad()).add(vehicle);
-    }
-
-    public ResponseDto processSimulation(RequestDto requestData) {
-        ResponseDto resultDto = new ResponseDto();
-        for (Command command : requestData.getCommands()) {
-            command.execute(this, resultDto);
-        }
-
-        return resultDto;
-    }
 }
 
 
